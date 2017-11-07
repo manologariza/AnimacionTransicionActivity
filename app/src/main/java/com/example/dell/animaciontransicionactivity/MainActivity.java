27639373e32c69bedc02f0b211cf4c3a689efc86
manologariza <32630874+manologariza@users.noreply.github.com>
2017-11-07 09:47:48 +0100

    Initial commit

---
+++ app/src/main/java/com/example/dell/animaciontransicionactivity/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.dell.animaciontransicionactivity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+       public void forwardZoom(View button)
+    {
+        startActivity(new Intent(this, SecondActivity.class));
+        overridePendingTransition(R.anim.zoom_forward_in, R.anim.zoom_forward_out);
+    }
+
+    public void left(View button)
+    {
+        startActivity(new Intent(this, SecondActivity.class));
+        overridePendingTransition(R.anim.left_in, R.anim.left_out);
+    }
+
+    public void fade(View button)
+    {
+        startActivity(new Intent(this, SecondActivity.class));
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+    }
+
+    public void byDefault(View button)
+    {
+        //En este caso no utilizamos método overridePendingTransition ya que queremos que haga la animación por defecto
+        startActivity(new Intent(this, SecondActivity.class));
+
+        //Se parecerá a left_in-left_out (los hacemos diferentes por ejemplo cambiando el tiempo de la transición)
+    }
+}
---
+++ app/src/main/java/com/example/dell/animaciontransicionactivity/SecondActivity.java
@@ -0,0 +1,42 @@
+package com.example.dell.animaciontransicionactivity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    public void zoomBack(View button)
+    {
+        startActivity(new Intent(this, MainActivity.class));
+        overridePendingTransition(R.anim.zoom_back_in, R.anim.zoom_back_out);
+    }
+
+    public void fade(View button)
+    {
+        startActivity(new Intent(this, MainActivity.class));
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+    }
+
+    public void right(View button)
+    {
+        startActivity(new Intent(this, MainActivity.class));
+        overridePendingTransition(R.anim.right_in, R.anim.right_out);
+    }
+
+    public void back(View button)
+    {
+        //No utilizamos ningún efecto de transición adicional sino que usamos el que hace el sistema por defecto
+        super.onBackPressed();
+
+        //Se parecerá a right_in-right_out (los hacemos diferenetes por ejemplo cambiando el tiempo de la transición)
+
+    }
+}
